e117eac6f4cf1e9d2a95f756676a5ae5d58d11f8
liveris-dev <liverispapa@gmail.com>
2025-10-01 17:37:07 +0300

    Add authentication, flashing messages, admin privileges

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from enum import unique
-
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, abort, flash
 import smtplib
 import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, Text
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired, URL
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, SubmitField, SelectField, PasswordField
+from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditor, CKEditorField
 from flask_bootstrap import Bootstrap5
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
+from functools import wraps
 
 my_email = os.environ.get('EMAIL_ADDRESS')
 my_password = os.environ.get('EMAIL_KEY')
@@ -23,17 +24,26 @@ ckeditor = CKEditor(app)
 #CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-#ADD ENV VARIABLE TO DB URI TO USE WITH POSTGRESQL
+#Add env variable to db uri to use with postgresql on render
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI', 'sqlite:///projects.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+
+#CONFIGURE FLASK LOGIN MANAGER
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+#CREATE USER LOADER CALLBACK
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, int(user_id))
 
 #CONFIGURE TABLES
 class Project(db.Model):
     __tablename__= "projects"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     # Create a foreign key, "users.id" the users refers to the tablename of User.
-    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    author_id: Mapped[int] = mapped_column(db.ForeignKey("users.id"))
     # Create reference to the User object. The "projects" refers to the projects property in the User class.
     author = relationship("User", back_populates="projects")
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
@@ -43,7 +53,7 @@ class Project(db.Model):
     github: Mapped[str] = mapped_column(String(250), nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
@@ -55,7 +65,7 @@ class User(db.Model):
 with app.app_context():
     db.create_all()
 
-#CREATE WTFORM FOR NEW PROJECT
+#CREATE WTFORMS
 class ProjectForm(FlaskForm):
     title=StringField(label='Project Title', validators=[DataRequired()])
     subtitle=StringField(label='Project Subtitle', validators=[DataRequired()])
@@ -64,6 +74,18 @@ class ProjectForm(FlaskForm):
     project_url=StringField(label='Github Link', validators=[DataRequired(), URL()])
     img_url=StringField(label='Image URL', validators=[DataRequired(), URL()])
     submit=SubmitField(label='Submit Project')
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Let Me In!")
+
 
 #CREATE SEND EMAIL FUNCTION FOR CONTACT
 def send_email(name, email, phone, message):
@@ -77,24 +99,56 @@ def send_email(name, email, phone, message):
             msg=email_message
         )
 
-#ADD A NEW PROJECT
-@app.route("/new_project", methods=["GET","POST"])
-def new_project():
-    form = ProjectForm()
-    if form.validate_on_submit():
-        new_project_to_add=Project(
-            title=form.title.data,
-            subtitle=form.subtitle.data,
-            body=form.body.data,
-            type=form.category.data,
-            github=form.project_url.data,
-            img_url=form.img_url.data
-        )
-        db.session.add(new_project_to_add)
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template("add-project.html", form=form)
+#CREATE ADMIN REQUIRED DECORATOR
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args,**kwargs):
+        if current_user.is_anonymous or current_user.id != 1:
+            return abort(403)
+        return f(*args,**kwargs)
+    return decorated_function
 
+@app.route("/register", methods=["GET","POST"])
+def register_user():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        registered_user = db.session.execute(db.select(User).where(User.email == form.email.data)).scalar()
+        if registered_user:
+            return redirect(url_for("login"))
+        hashed_salted_password = generate_password_hash(password=form.password.data, method="pbkdf2", salt_length=8)
+        new_user = User(
+            email=form.email.data,
+            name = form.name.data,
+            password = hashed_salted_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for("home"))
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        registered_user = db.session.execute(db.select(User).where(User.email == form.email.data)).scalar()
+        if registered_user:
+            if check_password_hash(pwhash=registered_user.password, password=form.password.data):
+                login_user(registered_user)
+                return redirect(url_for("home"))
+            else:
+                flash("The password is incorrect. Please try again.", category="error")
+                return redirect(url_for("login"))
+        else:
+            flash("The email you entered is not registered. Please sign up first.", category="error")
+            return redirect(url_for('register_user'))
+    return render_template("login.html", form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route("/")
@@ -112,7 +166,28 @@ def show_project(project_id):
     requested_project = db.get_or_404(Project, project_id)
     return render_template("project.html", project=requested_project)
 
+#ADD A NEW PROJECT
+@app.route("/new_project", methods=["GET","POST"])
+@admin_only
+def new_project():
+    form = ProjectForm()
+    if form.validate_on_submit():
+        new_project_to_add=Project(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            type=form.category.data,
+            github=form.project_url.data,
+            img_url=form.img_url.data,
+            author_id=current_user.id
+        )
+        db.session.add(new_project_to_add)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("add-project.html", form=form)
+
 @app.route("/terminate/<project_id>")
+@admin_only
 def terminate_project(project_id):
     requested_project = db.get_or_404(Project, project_id)
     db.session.delete(requested_project)
